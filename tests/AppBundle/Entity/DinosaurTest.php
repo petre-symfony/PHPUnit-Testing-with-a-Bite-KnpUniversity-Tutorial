16e2a1094d90b4c94a2ac60a0e1457a91986890c
petrero <petrero@gmail.com>
2018-01-17 20:25:57 +0200

    Testing for Bugs Let's add one more quickly: imagine that a bug has been reported! Gasp! People are saying that if they create a Dinosaur of length 15, by the time it is born and grows up, it's smaller than 15! The dinos are shrinking! Probably a good thing. Let's add a test for this
    Ok, try the test!
    
    ./vendor/bin/phpunit
    Because our code is actually perfect, it passes!

--- tests/AppBundle/Entity/DinosaurTest.php
+++ tests/AppBundle/Entity/DinosaurTest.php
@@ -14,4 +14,11 @@ class DinosaurTest extends TestCase {
     $dinosaur->setLength(9);
     $this->assertSame(9, $dinosaur->getLength());
   }
+  
+  public function testDinosaurHasNotShrunk(){
+    $dinosaur = new Dinosaur();
+    $dinosaur->setLength(15);
+    
+    $this->assertGreaterThan(12, $dinosaur->getLength(), 'Did you put it in the washing machine?');
+  }
 }
