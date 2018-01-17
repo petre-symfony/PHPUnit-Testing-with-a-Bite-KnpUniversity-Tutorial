82fde5f1fea0801017720fee9ba3b60a94b55043
petrero <petrero@gmail.com>
2018-01-17 19:46:58 +0200

    Write a test that certifies that our PHPUnit instalation works.
    Test Rules & Best Practices
    Ok, let's talk about the basic rules of writing a PHPUnit test. First, you can technically put your test classes anywhere... but.. you've gotta admit that tests/ is a pretty good place. Actually, in a Symfony project, you automatically start with a phpunit.xml.dist file. Well, in Symfony 4, this is added when you install phpunit. We'll talk more about this file later... but PHPUnit reads this automatically. And... check it out! It says that our tests all live in... tests/. That's how PHPUnit is able to find our DinosaurTest.
    
    Second, our test has a namespace... but that's not really important. In a Symfony project, your composer.json file has an autoload-dev section that basically says that anything in tests/ should start with the namespace Tests. No big deal.
    
    Let's get to the really important stuff, because PHPUnit does have a few crucial rules. First, your test class must extend TestCase and end in the word Test. Second, all of your test methods must be public and start with the word test. When you run PHPUnit, it basically looks for all classes ending in Test and all public functions inside starting with test.

---
+++ tests/AppBundle/Entity/DinosaurTest.php
@@ -0,0 +1,10 @@
+<?php
+namespace tests\AppBundle\Entity;
+
+use PHPUnit\Framework\TestCase;
+
+class DinosaurTest extends TestCase {
+  public function testThatYourComputerWorks(){
+    $this->assertTrue(FALSE);
+  }
+}
