cce9c518d216d3821101cdcace27c8ecdd4711ca
petrero <petrero@gmail.com>
2018-01-18 18:37:24 +0200

    Refactor! And that means it's time to refactor! Since we're going to eventually create a lot of dinosaurs, let's create a new private function called createDinosaur() with three arguments: genus, isCarnivorous and length. Use those on the new Dinosaur().
    And because this has a test, it will tell us if we made any mistakes. But I doubt that... try the test!
    
    ./vendor/bin/phpunit
    Explosion!
    
    Return value of DinosaurFactory::growVelociraptor must be a dinosaur: null returned

--- src/AppBundle/Factory/DinosaurFactory.php
+++ src/AppBundle/Factory/DinosaurFactory.php
@@ -5,10 +5,12 @@ use AppBundle\Entity\Dinosaur;
 
 class DinosaurFactory {
   public function growVelociraptor(int $length): Dinosaur{
-    $dinosaur = new Dinosaur('Velociraptor', true);
+    return $this->createDinosaur('Velociraptor', true, $length);
+  }
+  
+  private function createDinosaur(string $genus, bool $isCarnivorous, int $length) {
+    $dinosaur = new Dinosaur($genus, $isCarnivorous);
     
     $dinosaur->setLength($length);
-    
-    return $dinosaur;
   }
 }
