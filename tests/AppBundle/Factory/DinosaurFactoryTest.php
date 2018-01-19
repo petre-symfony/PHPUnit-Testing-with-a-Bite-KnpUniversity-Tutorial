2832303ec6365f40d6ce05a114f1feeaa6bbf2d2
petrero <petrero@gmail.com>
2018-01-19 20:09:25 +0200

    We Want HUGE Dinosaurs And management already loves this feature. But... they don't think the dinosaurs are big enough. Now, they want to use the word 'huge' to grow mouth-gaping dinosaurs! They've gone mad!
    No problem! Thanks to the power of data providers, we can just add more test cases! Or... if you feel like this method is already doing enough, you can create another test.
    Oh, but we need to define what huge means. Back in Dinosaur, add const HUGE = 30. And management decided to make the large dinosaurs a bit smaller - set LARGE to 10. Use the constant in the test and compare it.
    
    Huge Data Provider
    With the test function done, create the data provider: getHugeDinosaurSpecTests(). Just like before, make this return an array. Each individual test case will also be an array like last time, but now with only one item inside. Test for 'huge dinosaur, then also huge dino, just the word huge and, of course, OMG and... the scream Emoji!
    Back on the test method, connect it to the provider: @dataProvider getHugeDinosaurSpecTests.
    
    Ok, let's watch some tests fail! Go Sebastian go!
    
    ./vendor/bin/phpunit
    Beautiful failures! Five new test cases and five new failures. Time to code!

--- tests/AppBundle/Factory/DinosaurFactoryTest.php
+++ tests/AppBundle/Factory/DinosaurFactoryTest.php
@@ -52,12 +52,32 @@ class DinosaurFactoryTest extends TestCase{
     $this->assertSame($expectedIsCarnivorous, $dinosaur->isCarnivorous(), 'Diets do not match');
   }
   
+
   public function getSpecificationTests(){
     return [
       // specification, is large, is carnivorous
       ['large carnivorous dinosaur', true, true ],
       'default response' => ['give me all the cookies', false, false ],
       ['large herbivore', true, false ]
+    ];
+  }
+  
+  /**
+    * @dataProvider getHugeDinosaurSpecTests
+  */
+  public function testItGrowsAHugeDinosaur(string $specification){
+    $dinosaur = $this->factory->growFromSpecification($specification);
+    
+    $this->assertGreaterThanOrEqual(Dinosaur::HUGE, $dinosaur->getLength());
+  }
+  
+  public function getHugeDinosaurSpecTests(){
+    return [
+      ['huge dinosaur'],
+      ['huge dino'],
+      ['huge'],
+      ['OMG'],
+      ['😱']  
     ];
   }
 }
