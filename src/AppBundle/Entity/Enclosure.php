947067c2a394dd64effc3091c5f3c4ec58c4ce43
petrero <petrero@gmail.com>
2018-01-19 21:11:24 +0200

    Adding the Annotations Before we keep going, since the tests are green, let's add the missing Doctrine annotations. With my cursor inside Enclosure, I'll go to the Code->Generate menu - or Command+N on a mac - and select 'ORM Class'. That's just a shortcut to add the annotations above the class. Now, above the  property, use @ORM\OneToMany with targetEntity='Dinosaur', mappedBy='enclosure' - we'll add that property in a moment - and cascade={'persist'}. In Dinosaur, add the other side: private  with @ORM\ManyToOne. Point back to the Enclosure class with inversedBy='dinosaurs'. That should not have broken anything... but run the tests to be sure!
    ./vendor/bin/phpunit

--- src/AppBundle/Entity/Enclosure.php
+++ src/AppBundle/Entity/Enclosure.php
@@ -4,9 +4,14 @@ namespace AppBundle\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="enclosure")
+ */
 class Enclosure {
   /**
    * @var Collection
+   * @ORM\OneToMany(targetEntity="AppBundle\Entity\Dinosaur", mappedBy="enclosure", cascade=["persist"])
    */
   private $dinosaurs;
   
