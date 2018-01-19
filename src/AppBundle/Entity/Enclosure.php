9d8a5c0ad994086e25b51f46efa7e70dc672b93a
petrero <petrero@gmail.com>
2018-01-19 21:56:51 +0200

    Awesome! Let's go throw that exception! Inside Enclosure, at the bottom, add a new private function called canAddDinosaur with a Dinosaur argument. This will return a bool. If the enclosure is empty, then it's definitely ok to add a dinosaur. Make sure the class extends \Exception. Now throw that exception! Check the tests!
    ./vendor/bin/phpunit
    Woo! We got it!

--- src/AppBundle/Entity/Enclosure.php
+++ src/AppBundle/Entity/Enclosure.php
@@ -4,6 +4,7 @@ namespace AppBundle\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use AppBundle\Entity\Dinosaur;
+use AppBundle\Exception\NotABuffetException;
 
 /**
  * @ORM\Entity
@@ -25,6 +26,15 @@ class Enclosure {
   }
   
   public function addDinosaur(Dinosaur $dinosaur){
+    if (!$this->canAddDinosaur($dinosaur)){
+      throw new NotABuffetException();
+    }
+    
     $this->dinosaurs[] = $dinosaur;
+  }
+  
+  private function canAddDinosaur(Dinosaur $dinosaur): bool {
+    return count($this->dinosaurs) === 0 
+      || $this->dinosaurs->first()->isCarnivorous() === $dinosaur->isCarnivorous();
   }
 }
--- src/AppBundle/Exception/NotABuffetException.php
+++ src/AppBundle/Exception/NotABuffetException.php
@@ -2,6 +2,6 @@
 namespace AppBundle\Exception;
 
 
-class NotABuffetException {
+class NotABuffetException extends \Exception{
   protected $message = 'Please do not mix the carnivorous and non-carnivorous dinosaurs. It will be a massacre!';
 }
