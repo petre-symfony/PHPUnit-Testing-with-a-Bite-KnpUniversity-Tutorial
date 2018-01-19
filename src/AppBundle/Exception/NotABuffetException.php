f51c66ec20893d47005c564adb1ce7c09390f9b8
petrero <petrero@gmail.com>
2018-01-19 21:43:59 +0200

    The dinosaur enclosures are looking great. There's just one minor problem: people keep accidentally putting nice, gentle veggie-eating dinosaurs into the same enclosure as flesh-eating carnivores. The result is... well... expensive, and there's a lot of cleanup too.
    We need to prevent the meat eaters and the veggie eaters from being mixed inside the same enclosure. In other words, if somebody tries to do this, we need to throw an exception!
    
    The Custom Exception Class
    It's optional, but let's create a custom exception class for this: NotABuffetException. I'll even give this a default message: people need to understand how horrible this is!
    
    Making sure that this exception is thrown at the right time is critical to business. So let's write a test: testItDoesNotAllowCarnivorousDinosaursToMixWithHerbivores.
    
    Inside the method, create this terrifying situation. By default, dinosaurs are non-carnivorous.
    
    Expecting an Exception
    At this point, an exception should be thrown. So... how can we test for that? By telling PHPUnit to expect an exception with... well... expectException() and then the exception class: NotABuffetException::class.
    
    If we've done our work correctly, this should fail. Try the test!
    
    ./vendor/bin/phpunit
    Yes! Failed asserting that exception of type NotABuffetException is thrown.

---
+++ src/AppBundle/Exception/NotABuffetException.php
@@ -0,0 +1,7 @@
+<?php
+namespace AppBundle\Exception;
+
+
+class NotABuffetException {
+  protected $message = 'Please do not mix the carnivorous and non-carnivorous dinosaurs. It will be a massacre!';
+}
--- tests/AppBundle/Entity/EnclosureTest.php
+++ tests/AppBundle/Entity/EnclosureTest.php
@@ -4,6 +4,7 @@ namespace tests\AppBundle\Entity;
 use PHPUnit\Framework\TestCase;
 use AppBundle\Entity\Enclosure;
 use AppBundle\Entity\Dinosaur;
+use AppBundle\Exception\NotABuffetException;
 
 class EnclosureTest extends TestCase{
   public function testItHasNoDinosaursByDefault(){
@@ -19,5 +20,14 @@ class EnclosureTest extends TestCase{
     $enclosure->addDinosaur(new Dinosaur());
     
     $this->assertCount(2, $enclosure->getDinosaurs());
+  }
+  
+  public function testItDoesNotAllowCarnivorousDinosaursToMixWithHerbivores(){
+    $enclosure = new Enclosure();
+    $enclosure->addDinosaur(new Dinosaur());
+    
+    $this->expectException(NotABuffetException::class);
+    
+    $enclosure->addDinosaur(new Dinosaur('Velociraptor', true));
   }
 }
