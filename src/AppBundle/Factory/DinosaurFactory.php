49db3079c710a31f9dc9e78c609add60a1928a52
petrero <petrero@gmail.com>
2018-01-19 19:26:56 +0200

    Ok, let's code! And remember: don't over-think things: just focus on getting each test to pass. Let's start with the second test, the default values.
    Attacking Test #1
    Inside DinosaurFactory, I'll paste a few default values: we'll use codeName as the genus, because these are experimental dinosaurs, set the length to be a small dinosaur, and create leaf-eating friends.
    Yep, with these values, our second test should be happy.
    Ok, that should be enough to get one test to pass. Run 'em:
    
    ./vendor/bin/phpunit
    Yes! Failure... dot... failure.

--- src/AppBundle/Factory/DinosaurFactory.php
+++ src/AppBundle/Factory/DinosaurFactory.php
@@ -9,8 +9,16 @@ class DinosaurFactory {
   }
   
   public function growFromSpecification(string $specification): Dinosaur{
-      
+    //defaults
+    $codeName = 'ING-' . random_int(1, 99999);
+    $length = random_int(1, Dinosaur::LARGE - 1);
+    $IsCarnivorous = FALSE;
+    
+    $dinosaur = $this->createDinosaur($codeName, $IsCarnivorous, $length);
+    
+    return $dinosaur;
   }
+  
   private function createDinosaur(string $genus, bool $isCarnivorous, int $length) {
     $dinosaur = new Dinosaur($genus, $isCarnivorous);
     
