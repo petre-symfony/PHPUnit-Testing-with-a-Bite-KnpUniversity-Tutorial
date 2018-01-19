e598bfe0aa0e8cce797b8e777b572b6d476cbefb
petrero <petrero@gmail.com>
2018-01-19 20:32:14 +0200

    Refactoring our Ugly Code And that means we've reached step 3 of TDD: refactor! I don't actually love ugly code - it's just that it wasn't time to worry about it yet. TDD helps you focus on writing your business logic correctly first, and then on improving the code.
    So let's makes this better. Actually, if you downloaded the course code, then you should have a tutorial/ directory with a DinosaurFactory.php file inside. Copy the private function from that file, find our DinosaurFactory, and paste at the bottom.
    
    This is still a bit complex, but it removes the duplication and makes the length calculation more systematic. Copy the method name, scroll up, delete all that ugly length logic... and just say
    
    My new code probably doesn't contain any bugs... but you should totally not trust me! I mess up all the time! Just run the tests.
    
    ./vendor/bin/phpunit
    Ha! It works! And you doubted me....
    
    Next! What if you need to test that a method throws an exception under certain conditions? Like... if you try to put a T-Rex in the same enclosure as a nice, friendly Brontosaurus. Let's find out!

--- src/AppBundle/Factory/DinosaurFactory.php
+++ src/AppBundle/Factory/DinosaurFactory.php
@@ -11,24 +11,9 @@ class DinosaurFactory {
   public function growFromSpecification(string $specification): Dinosaur{
     //defaults
     $codeName = 'ING-' . random_int(1, 99999);
-    $length = random_int(1, Dinosaur::LARGE - 1);
+    $length = $this->getLengthFromSpecification($specification);
     $isCarnivorous = FALSE;
     
-    if (stripos($specification, 'huge') !== false) {
-      $length = random_int(Dinosaur::HUGE, 100);
-    }
-    
-    if (stripos($specification, 'OMG') !== false) {
-      $length = random_int(Dinosaur::HUGE, 100);
-    }
-    
-    if (stripos($specification, '😱') !== false) {
-      $length = random_int(Dinosaur::HUGE, 100);
-    }
-    
-    if (stripos($specification, 'large') !== false) {
-      $length = random_int(Dinosaur::LARGE, Dinosaur::HUGE - 1);
-    }
     
     if (stripos($specification, 'carnivorous') !== false) {
       $isCarnivorous = true;
@@ -44,5 +29,29 @@ class DinosaurFactory {
     $dinosaur->setLength($length);
     
     return $dinosaur;
+  }
+  
+  private function getLengthFromSpecification(string $specification): int {
+    $availableLengths = [
+      'huge' => ['min' => Dinosaur::HUGE, 'max' => 100],
+      'omg' => ['min' => Dinosaur::HUGE, 'max' => 100],
+      '😱' => ['min' => Dinosaur::HUGE, 'max' => 100],
+      'large' => ['min' => Dinosaur::LARGE, 'max' => Dinosaur::HUGE - 1],
+    ];
+    $minLength = 1;
+    $maxLength = Dinosaur::LARGE - 1;
+
+    foreach (explode(' ', $specification) as $keyword) {
+      $keyword = strtolower($keyword);
+
+      if (array_key_exists($keyword, $availableLengths)) {
+        $minLength = $availableLengths[$keyword]['min'];
+        $maxLength = $availableLengths[$keyword]['max'];
+
+        break;
+      }
+    }
+
+    return random_int($minLength, $maxLength);
   }
 }
