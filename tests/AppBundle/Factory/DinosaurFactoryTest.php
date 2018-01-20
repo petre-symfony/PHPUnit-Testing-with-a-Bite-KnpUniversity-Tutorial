79f8c35d14731d00e68913fee654b995c0cd9836
petrero <petrero@gmail.com>
2018-01-20 15:42:13 +0200

    Copy code from branch 14

---
+++ tests/AppBundle/Factory/DinosaurFactoryTest.php
@@ -0,0 +1,70 @@
+<?php
+namespace tests\AppBundle\Factory;
+
+use PHPUnit\Framework\TestCase;
+use AppBundle\Entity\Dinosaur;
+use AppBundle\Factory\DinosaurFactory;
+use AppBundle\Service\DinosaurLengthDeterminator;
+
+class DinosaurFactoryTest extends TestCase{
+  /**
+   * @var DinosaurFactory
+   */
+  private $factory;
+  
+  /**
+   *
+   * @var \PHPUnit_Framework_MockObject_MockObject
+   */
+  private $lengthDeterminator;
+  
+  public function setUp(){
+    $this->lengthDeterminator = $this->createMock(DinosaurLengthDeterminator::class); 
+    $this->factory = new DinosaurFactory($this->lengthDeterminator);
+  }
+
+  public function testItGrowsAVelociraptor(){
+    $dinosaur = $this->factory->growVelociraptor(5);
+    
+    $this->assertInstanceOf(Dinosaur::class, $dinosaur);
+    $this->assertInternalType('string', $dinosaur->getGenus());
+    $this->assertSame('Velociraptor', $dinosaur->getGenus());
+    $this->assertSame(5, $dinosaur->getLength());
+  }
+  
+  public function testItGrowsATriceratops(){
+    $this->markTestIncomplete('Waiting for confirmation from GenLab');
+  }
+  
+  public function testItGrowsABabyVelociraptor(){
+    if (!class_exists('Nanny')){
+      $this->markTestSkipped('There is nobody to watch the baby raptor');
+    }
+    
+    $dinosaur = $this->factory->growVelociraptor(1);
+    $this->assertSame(1, $dinosaur->getLength());
+  }
+  
+  /**
+    * @dataProvider getSpecificationTests
+  */
+  public function testItGrowsADinosaurFromSpecification(string $spec, bool $expectedIsCarnivorous){
+    $this->lengthDeterminator->method('getLengthFromSpecification')
+      ->willReturn(20);  
+      
+    $dinosaur = $this->factory->growFromSpecification($spec);
+    
+    $this->assertSame($expectedIsCarnivorous, $dinosaur->isCarnivorous(), 'Diets do not match');
+    $this->assertSame(20, $dinosaur->getLength());
+  }
+  
+
+  public function getSpecificationTests(){
+    return [
+      // specification, is carnivorous
+      ['large carnivorous dinosaur', true ],
+      'default response' => ['give me all the cookies', false ],
+      ['large herbivore', false ]
+    ];
+  }
+}
