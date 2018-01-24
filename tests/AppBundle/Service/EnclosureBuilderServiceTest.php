195efbada100e4f48e3d0af94a9a8a8218b04091
petrero <petrero@gmail.com>
2018-01-24 18:45:04 +0200

    Back in the test, add {dollar_sign}em->expects({dollar_sign}this->once()) with ->method('persist'). We know that this should be called with an instance of an Enclosure object. We don't know exactly which Enclosure object, but we can check the type. Try the test!
    ./vendor/bin/phpunit
    There's the failure: persist should be called 1 time, but was called 0 times.

--- tests/AppBundle/Service/EnclosureBuilderServiceTest.php
+++ tests/AppBundle/Service/EnclosureBuilderServiceTest.php
@@ -6,10 +6,16 @@ use AppBundle\Service\EnclosureBuilderService;
 use Doctrine\ORM\EntityManagerInterface;
 use AppBundle\Factory\DinosaurFactory;
 use AppBundle\Entity\Dinosaur;
+use AppBundle\Entity\Enclosure;
 
 class EnclosureBuilderServiceTest extends TestCase{
   public function testItBuildsAndPersistsEnclosure(){
     $em = $this->createMock(EntityManagerInterface::class); 
+    
+    $em->expects($this->once())
+      ->method('persist')
+      ->with($this->isInstanceOf(Enclosure::class));
+    
     $dinoFactory = $this->createMock(DinosaurFactory::class);
     
     $dinoFactory->expects($this->exactly(2))
@@ -22,6 +28,5 @@ class EnclosureBuilderServiceTest extends TestCase{
     
     $this->assertCount(1, $enclosure->getSecurities());
     $this->assertCount(2, $enclosure->getDinosaurs());
-    dump($enclosure->getDinosaurs()->toArray());
   }    
 }
