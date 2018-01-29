f05e10100fb3d96af459a6214330fd9a98a9bfe1
petrero <petrero@gmail.com>
2018-01-29 18:10:22 +0200

    Clearing the Database before Tests At this point, not much has changed really. Our tests will still pass one time, but will fail each time after. We haven't actually fixed the problem yet!
    How can we? The best way is to fully empty the database at the beginning of each test. This would certainly put our database into a known state: empty! Then, if we do need any data before running the test, we can manually add it in the test. It's not super fancy, but it keeps everything really clear.
    
    Like most good things in life, there are two ways to do this. First, if you downloaded the course code, then in the tutorial/tests directory, you'll find an EnclosureBuilderServiceIntegrationTest.php file. Copy the truncateEntities() method and paste that into your test class.
    This is simple: pass the method an array of entities and it will empty them.
    
    You might want to call this at the top of every test method. But another great option is to override setUp() and add it there. Let's empty all three entities: Enclosure, Security and Dinosaur.
    
    For this method to work, we need a getEntityManager() method. At the bottom, add private function getEntityManager(). Then, copy our logic from above, paste it here, and add return. And since you know I love auto-completion, I'll add a @return EntityManager.
    This makes truncateEntities() happy! And we can even use getEntityManager() above.
    
    Oh, and it's really important that we call self::bootKernel() before we try to access any services. The best thing to do is remove it from the test method and add it to setUp().
    
    Done! Try the tests:
    
    ./vendor/bin/phpunit --filter testItBuildsEnclosureWithTheDefaultSpecification
    We got it! We can run them over, and over, and over again. Always green!

--- tests/AppBundle/Service/EnclosureBuilderServiceIntegrationTest.php
+++ tests/AppBundle/Service/EnclosureBuilderServiceIntegrationTest.php
@@ -6,19 +6,27 @@ use AppBundle\Service\EnclosureBuilderService;
 use Doctrine\ORM\EntityManager;
 use AppBundle\Entity\Security;
 use AppBundle\Entity\Dinosaur;
+use AppBundle\Entity\Enclosure;
 
 class EnclosureBuilderServiceIntegrationTest extends KernelTestCase{
-  public function testItBuildsEnclosureWithDefaultSpecification(){
+  public function setUp() {
     self::bootKernel();
+    
+    $this->truncateEntities([
+      Enclosure::class,
+      Dinosaur::class,
+      Security::class
+    ]);
+  }
+
+  public function testItBuildsEnclosureWithDefaultSpecification(){
     
     /** @var EnclosureBuilderService $enclosureBuilderService */
     $enclosureBuilderService = self::$kernel->getContainer()
       ->get('test.'.EnclosureBuilderService::class);
     $enclosureBuilderService->buildEnclosure();
     
-    /** @var EntityManager $em */
-    $em = self::$kernel->getContainer()
-      ->get('doctrine')->getManager();
+    $em = $this->getEntityManager();
     
     $count = (int) $em->getRepository(Security::class)
       ->createQueryBuilder('s')
@@ -35,5 +43,33 @@ class EnclosureBuilderServiceIntegrationTest extends KernelTestCase{
       ->getSingleScalarResult();
     
     $this->assertSame(3, $count, 'Amount of dinosaurs is not the same');
+  }
+  
+  private function truncateEntities(array $entities){
+    $connection = $this->getEntityManager()->getConnection();
+    $databasePlatform = $connection->getDatabasePlatform();
+
+    if ($databasePlatform->supportsForeignKeyConstraints()) {
+        $connection->query('SET FOREIGN_KEY_CHECKS=0');
+    }
+
+    foreach ($entities as $entity) {
+        $query = $databasePlatform->getTruncateTableSQL(
+            $this->getEntityManager()->getClassMetadata($entity)->getTableName()
+        );
+
+        $connection->executeUpdate($query);
+    }
+
+    if ($databasePlatform->supportsForeignKeyConstraints()) {
+        $connection->query('SET FOREIGN_KEY_CHECKS=1');
+    }
+  }
+  
+  /** @return EntityManager */
+  private function getEntityManager(){
+    return self::$kernel->getContainer()
+      ->get('doctrine')
+      ->getManager();   
   }
 }
