f9a8dcd8a02606af7a6bfc7292a8eca43eee77cc
petrero <petrero@gmail.com>
2018-01-30 19:26:43 +0200

    Copy code from branch 23

---
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace tests\AppBundle\Controller;
+
+use Liip\FunctionalTestBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase {
+  public function testEnclosuresAreShownOnTheHomepage() {
+    $client = $this->makeClient();
+    
+    $crawler = $client->request('GET', '/');
+    
+    $this->assertStatusCode(200, $client);
+  }
+}
