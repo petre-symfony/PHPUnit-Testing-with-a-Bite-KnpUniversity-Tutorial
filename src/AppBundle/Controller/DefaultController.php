bead74d2c3a1796af605d183f846cf355d9a973b
petrero <petrero@gmail.com>
2018-01-31 20:37:13 +0200

    For the submit logic, go back into the tutorial/ directory, find DefaultController and copy all of the growAction() method. Paste this into our DefaultController. Oh, and we need a few use statements: re-type part of @Method and hit tab to add its use statement. Do the same for DinosaurFactory.
    Ok, it's happy! Sure, the code is lacking the normal security and safeguards we expect when using Symfony's form system... but it's only a dinosaur park people! We do, however, have the success flash message!
    
    So if we haven't messed anything up, it should work. Try the test!
    
    ./vendor/bin/phpunit --filter testItGrowsADinosaurFromSpecification
    Yes! It passes! We just confirmed that this form works before we ever even loaded it in a browser. That's pretty cool.
    
    So that's the power of functional tests. And I find them especially powerful when using Mink and testing that my JavaScript works.
    
    Ok guys, just one more topic left, and it's fun! Continuous integration! You know, the fancy term that means: let the robots run your tests automatically!

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,6 +6,8 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use AppBundle\Entity\Enclosure;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use AppBundle\Factory\DinosaurFactory;
 
 class DefaultController extends Controller
 {
@@ -21,5 +23,35 @@ class DefaultController extends Controller
         return $this->render('default/index.html.twig', [
           'enclosures' => $enclosures    
         ]);
+    }
+    
+    // copy JUST this method :)
+
+    /**
+     * @Route("/grow", name="grow_dinosaur")
+     * @Method({"POST"})
+     */
+    public function growAction(Request $request, DinosaurFactory $dinosaurFactory)
+    {
+        $manager = $this->getDoctrine()->getManager();
+
+        $enclosure = $manager->getRepository(Enclosure::class)
+            ->find($request->request->get('enclosure'));
+
+        $specification = $request->request->get('specification');
+        $dinosaur = $dinosaurFactory->growFromSpecification($specification);
+
+        $dinosaur->setEnclosure($enclosure);
+        $enclosure->addDinosaur($dinosaur);
+
+        $manager->flush();
+
+        $this->addFlash('success', sprintf(
+            'Grew a %s in enclosure #%d',
+            mb_strtolower($specification),
+            $enclosure->getId()
+        ));
+
+        return $this->redirectToRoute('homepage');
     }
 }
